1be9713c043445a7a5ae52ea8c9cf0de7f31fd10
Jeremy Foster <jeremy.foster@live.com>
2017-05-11 11:46:50 -0700

    remove .js and .js.map files

--- node/src/app/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-Object.defineProperty(exports, "__esModule", { value: true });
-// App
-__export(require("./app.module"));
-//# sourceMappingURL=index.js.map
